660edde3d25c49866baf4fa77f66d28fdeeccd14
Duong Yi <duonguet1903@gmail.com>
2022-09-09 22:27:45 +0700

    Adding a Bottom TabBar

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -9,33 +9,48 @@ class TabScreen extends StatefulWidget {
 }
 
 class _TabScreenState extends State<TabScreen> {
+  final List<Map<String, Object>> _pages = [
+    {
+      'page': CategoriesScreen(),
+      'title': 'Categories',
+    },
+    {
+      'page': FavoritesScreen(),
+      'title': 'Favorites',
+    },
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-        length: 2,
-        initialIndex: 0,
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('DeliMeals'),
-            bottom: const TabBar(
-              tabs: <Widget>[
-                Tab(
-                  icon: Icon(Icons.category),
-                  text: 'Categories',
-                ),
-                Tab(
-                  icon: Icon(Icons.star),
-                  text: 'Favorites',
-                ),
-              ],
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(_pages[_selectedPageIndex]['title'] as String),
+        ),
+        body: _pages[_selectedPageIndex]['page'] as Widget,
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: _selectPage,
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          unselectedItemColor: Colors.white,
+          selectedItemColor: Colors.black,
+          currentIndex: _selectedPageIndex,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.category),
+              label: 'Categories',
             ),
-          ),
-          body: TabBarView(
-            children: <Widget>[
-              CategoriesScreen(),
-              FavoritesScreen(),
-            ],
-          ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.star),
+              label: 'Favorites',
+            ),
+          ],
         ));
   }
 }
