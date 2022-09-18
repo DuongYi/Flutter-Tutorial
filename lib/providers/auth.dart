94c9b60bd3b25c935577ab57145933d8c127764f
Duong Yi <duonguet1903@gmail.com>
2022-09-18 15:56:08 +0700

    add Login

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -5,35 +5,56 @@ import 'package:http/http.dart' as http;
 import '../constants/index.dart';
 
 class Auth with ChangeNotifier {
-  final String _token = '';
-  final DateTime _expiryDate = DateTime.now();
-  final String _userId = '';
+  String _token = '';
+  DateTime _expiryDate = DateTime.now();
+  String _userId = '';
+
+  Future<void> _authenticate(
+    String email,
+    String password,
+    String urlSegment,
+  ) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=$API_KEY';
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      final responseData = json.decode(response.body);
+
+      print(responseData);
+
+      if (responseData['error'] != null) {
+        // ...
+      }
+      _token = responseData['idToken'];
+      _userId = responseData['localId'];
+      _expiryDate = DateTime.now().add(
+        Duration(
+          seconds: int.parse(
+            responseData['expiresIn'],
+          ),
+        ),
+      );
+
+      notifyListeners();
+    } catch (error) {
+      rethrow;
+    }
+  }
 
   Future<void> signup(String email, String password) async {
-    final url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=$API_KEY');
-
-    final response = await http.post(url,
-        body: json.encode({
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        }));
-
-    print(json.decode(response.body));
+    return _authenticate(email, password, 'signUp');
   }
 
   Future<void> login(String email, String password) async {
-    final url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=$API_KEY');
-
-    final response = await http.post(url,
-        body: json.encode({
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        }));
-
-    print(json.decode(response.body));
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
