906ed64547be30251984dbc47f78abb4b6ed4aba
Duong Yi <duonguet1903@gmail.com>
2022-09-19 11:54:12 +0700

    starting prj

---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class PlacedListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Places'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: const Center(child: CircularProgressIndicator()),
+    );
+  }
+}
