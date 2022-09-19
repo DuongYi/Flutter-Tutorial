177820095f99e0e81f983ec25bc3a2fb9173f685
Duong Yi <duonguet1903@gmail.com>
2022-09-19 10:56:55 +0700

    Animating Order Boxes

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -19,31 +19,37 @@ class _OrderItemState extends State<OrderItem> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(10),
-      child: Padding(
-        padding: const EdgeInsets.all(8),
-        child: Column(
-          children: <Widget>[
-            ListTile(
-              title: Text('\$${widget.order.amount.toStringAsFixed(2)}'),
-              subtitle: Text(
-                DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      height:
+          _expanded ? min(widget.order.products.length * 30.0 + 120, 200) : 110,
+      child: Card(
+        margin: const EdgeInsets.all(10),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                title: Text('\$${widget.order.amount.toStringAsFixed(2)}'),
+                subtitle: Text(
+                  DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
+                ),
+                trailing: IconButton(
+                  icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+                  onPressed: () {
+                    setState(() {
+                      _expanded = !_expanded;
+                    });
+                  },
+                ),
               ),
-              trailing: IconButton(
-                icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-                onPressed: () {
-                  setState(() {
-                    _expanded = !_expanded;
-                  });
-                },
-              ),
-            ),
-            if (_expanded)
-              Container(
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
                 padding:
                     const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-                height: min(widget.order.products.length * 30.0 + 10, 100),
+                height: _expanded
+                    ? min(widget.order.products.length * 30.0 + 10, 100)
+                    : 0,
                 child: ListView(
                   children: widget.order.products
                       .map(
@@ -73,7 +79,8 @@ class _OrderItemState extends State<OrderItem> {
                       .toList(),
                 ),
               ),
-          ],
+            ],
+          ),
         ),
       ),
     );
